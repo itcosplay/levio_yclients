85528a6ee16588e448c22774a94c223d10b32843
Konstantine Pialov <azure_dream@mail.ru>
2026-02-05 20:19:33 +0100

    Update

--- api/views/hai.py
+++ api/views/hai.py
@@ -1,13 +1,8 @@
-from django.utils.decorators import method_decorator
-from rest_framework.views import APIView
 from rest_framework.response import Response
-
-from api.decorators import require_levio_backend_origin
+from rest_framework.views import APIView
 
 
 class HiView(APIView):
-
-    @method_decorator(require_levio_backend_origin)
     def get(self, request):
-        
+
         return Response({"answer": "Hi from levio_yclients"})
